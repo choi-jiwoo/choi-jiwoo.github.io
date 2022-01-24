0cb61262e42faa273442c324efc4c4b8130ec11e
choi jiwoo <cho2.jiwoo@gmail.com>
2022-01-25 00:49:51 +0900

    Add Router component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './component/Navbar';
+import Cv from './page/Cv';
+import Home from './page/Home';
+import Portfolio from './page/Portfolio';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact element={<Home />} />
+          <Route path='/cv' exact element={<Cv />} />
+          <Route path='/portfolio' exact element={<Portfolio />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
