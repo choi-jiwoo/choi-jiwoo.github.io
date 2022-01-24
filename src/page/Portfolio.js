b04537c2beb01ec37d7b80abfabeeb4a78eed5e3
choi jiwoo <cho2.jiwoo@gmail.com>
2022-01-25 00:49:00 +0900

    Creat Portfolio component

---
+++ src/page/Portfolio.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Portfolio() {
+  return (
+    <div className='container-xl'>
+      <div className='text-4xl text-center py-16'>
+        🛠
+        <br />
+        Portfolio page
+        <br />
+        under construction
+      </div>
+    </div>
+  );
+}
+
+export default Portfolio;
