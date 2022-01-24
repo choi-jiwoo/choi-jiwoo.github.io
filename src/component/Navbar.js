c9f3e0ba287a3b78bca7e11399b53040bee9ef95
choi jiwoo <cho2.jiwoo@gmail.com>
2022-01-25 00:49:26 +0900

    Add Navbar component

---
+++ src/component/Navbar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div className='sticky w-full border-gray-900/10 bg-light'>
+      <nav className='container-xl navbar navbar-expand-lg navbar-light bg-light'>
+        <div className='container-fluid'>
+          <Link className='navbar-brand' to='/'>
+            Choi Ji Woo
+          </Link>
+          <button
+            className='navbar-toggler'
+            type='button'
+            data-bs-toggle='collapse'
+            data-bs-target='#navbarNavAltMarkup'
+            aria-controls='navbarNavAltMarkup'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+          >
+            <span className='navbar-toggler-icon'></span>
+          </button>
+          <div className='collapse navbar-collapse' id='navbarNavAltMarkup'>
+            <div className='navbar-nav'>
+              <Link className='nav-item nav-link' to='/cv'>
+                CV
+              </Link>
+              <Link className='nav-item nav-link' to='/portfolio'>
+                Portfolio
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
