fc0d6d8d26f04a13f3535539c6e7c5915eac6336
choi jiwoo <cho2.jiwoo@gmail.com>
2022-01-25 00:48:48 +0900

    Create Home component

---
+++ src/page/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className='container-xl'>
+      <div className='text-4xl text-center py-16'>
+        🛠
+        <br />
+        Home page
+        <br />
+        under construction
+      </div>
+    </div>
+  );
+}
+
+export default Home;
