ef24852267ff502893c101514944e968cf0bc406
choi jiwoo <cho2.jiwoo@gmail.com>
2022-01-25 00:48:41 +0900

    Create Cv component

---
+++ src/page/Cv.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Cv() {
+  return (
+    <div className='container-xl'>
+      <div className='text-4xl text-center py-16'>
+        🛠
+        <br />
+        CV page
+        <br />
+        under construction
+      </div>
+    </div>
+  );
+}
+
+export default Cv;
