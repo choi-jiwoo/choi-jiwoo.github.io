82531f5e00e1f6ffcc14ee7ee7ae7bc792957265
choi jiwoo <cho2.jiwoo@gmail.com>
2022-01-26 23:19:17 +0900

    Add About component

---
+++ src/component/About.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className='pt-16'>
+      <div className='font-extrabold text-4xl pb-3'>About Me</div>
+      <div className='leading-relaxed text-lg'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a lacus
+        et felis sodales luctus. Quisque id purus quam. Cras ac diam vitae dolor
+        hendrerit convallis vitae vitae augue. Donec tempor eros mauris,
+        hendrerit molestie arcu vehicula dapibus. Morbi eget ullamcorper lacus.
+        Fusce sed libero lorem. Aenean in turpis sollicitudin, rutrum odio sed,
+        rhoncus nulla. Quisque at ipsum facilisis, luctus libero a, sollicitudin
+        neque. Mauris a mattis dui, a tempor diam. Integer gravida pellentesque
+        felis ornare commodo. Duis vitae imperdiet odio. Aliquam ornare congue
+        vestibulum. In venenatis viverra suscipit. Proin at nunc vehicula,
+        mollis turpis id, interdum mauris. Duis mollis lobortis lobortis. Nunc
+        quis dui faucibus, convallis odio eget, ornare odio.
+      </div>
+    </div>
+  );
+}
+
+export default About;
--- src/page/Home.js
+++ src/page/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Profile from '../component/Profile';
+import About from '../component/About';
 
 function Home() {
   return (
     <div className='container-xl'>
       <Profile />
+      <div className='mx-48'>
+        <About />
+      </div>
     </div>
   );
 }
