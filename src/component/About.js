4180e851b6b9f1ef835e17542fbf8cd7b4fc01b7
choi jiwoo <cho2.jiwoo@gmail.com>
2022-02-14 17:34:44 +0900

    Update contents in About component

--- src/component/About.js
+++ src/component/About.js
@@ -3,19 +3,14 @@ import React from 'react';
 function About() {
   return (
     <div className='pt-32'>
-      <div className='font-extrabold text-4xl pb-3'>About Me</div>
+      <div className='font-extrabold text-4xl pb-3'>About</div>
       <div className='leading-relaxed text-lg indent-8'>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a lacus
-        et felis sodales luctus. Quisque id purus quam. Cras ac diam vitae dolor
-        hendrerit convallis vitae vitae augue. Donec tempor eros mauris,
-        hendrerit molestie arcu vehicula dapibus. Morbi eget ullamcorper lacus.
-        Fusce sed libero lorem. Aenean in turpis sollicitudin, rutrum odio sed,
-        rhoncus nulla. Quisque at ipsum facilisis, luctus libero a, sollicitudin
-        neque. Mauris a mattis dui, a tempor diam. Integer gravida pellentesque
-        felis ornare commodo. Duis vitae imperdiet odio. Aliquam ornare congue
-        vestibulum. In venenatis viverra suscipit. Proin at nunc vehicula,
-        mollis turpis id, interdum mauris. Duis mollis lobortis lobortis. Nunc
-        quis dui faucibus, convallis odio eget, ornare odio.
+        Currently studying for a bachelor's degree in e-business at Ajou
+        University in Republic of Korea. Interested in tech industry generally,
+        data engineering, data analytics, and how technology and data can bring
+        about change. And I want to be involved in the great changes that are
+        happening now with big data and AI technologies in many other industries
+        not only in IT.
       </div>
     </div>
   );
