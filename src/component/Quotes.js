99e6746d76d3e2fc8fa6bb1c89b478a06805f49b
choi jiwoo <cho2.jiwoo@gmail.com>
2022-02-14 19:27:30 +0900

    Add Quotes component after Profile component

---
+++ src/component/Quotes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Quotes() {
+  return (
+    <div className='pt-20 text-lg italic text-gray-500 flex flex-col items-center'>
+      <p>
+        "The people who are crazy enough to think they can change the world are
+        the one's who do"
+      </p>
+      <p>- Steve Jobs</p>
+    </div>
+  );
+}
+
+export default Quotes;
--- src/page/Home.js
+++ src/page/Home.js
@@ -3,11 +3,13 @@ import Profile from '../component/Profile';
 import About from '../component/About';
 import Timeline from '../component/Timeline';
 import Education from '../component/Education';
+import Quotes from '../component/Quotes';
 
 function Home() {
   return (
     <>
       <Profile />
+      <Quotes />
       <About />
       <Education />
       <Timeline />
