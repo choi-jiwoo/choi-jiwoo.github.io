ee85b4a158bcc2ebb9f279e036721680cb432881
choi jiwoo <cho2.jiwoo@gmail.com>
2022-02-16 00:13:09 +0900

    Modify styles in Quotes

--- src/component/Quotes.js
+++ src/component/Quotes.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 function Quotes() {
   return (
-    <div className='pt-20 text-lg italic text-gray-400 text-center mx-24'>
-      <p>"The people who are crazy enough to think</p>
-      <p>they can change the world are the one's who do"</p>
+    <div className='pt-20 text-lg italic text-gray-400 text-center max-w-md m-auto'>
+      <p>
+        "The people who are crazy enough to think they can change the world are
+        the one's who do"
+      </p>
       <p>- Steve Jobs</p>
     </div>
   );
