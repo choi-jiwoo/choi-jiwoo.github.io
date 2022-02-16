8d51f7ae311f73f7766af729a2dea744e38e1091
choi jiwoo <cho2.jiwoo@gmail.com>
2022-02-17 01:35:34 +0900

    Remove projects variable and call from local

--- src/page/Project.js
+++ src/page/Project.js
@@ -1,33 +1,13 @@
 import React from 'react';
 import Portfolio from '../component/Portfolio';
+import projects from '../asset/projects.json';
 
 function Project() {
-  const projects = [
-    {
-      name: 'financialdatapy',
-      url: 'https://github.com/choi-jiwoo/financialdatapy',
-    },
-    {
-      name: 'naverplacescraper',
-      url: 'https://github.com/choi-jiwoo/naver-place-scraper',
-    },
-    {
-      name: 'musinsa-ranking',
-      url: 'https://github.com/choi-jiwoo/musinsa-ranking',
-    },
-    {
-      name: 'capstone project - React',
-      url: 'https://github.com/choi-jiwoo/capstone-project-react',
-    },
-    {
-      name: 'capstone project - Django',
-      url: 'https://github.com/choi-jiwoo/capstone-project-django',
-    },
-  ];
+  const personalProject = projects.slice(0, 3);
+  const capstoneProject = projects.slice(3, 5);
 
   return (
     <div className='project'>
-      <Portfolio list={projects} />
     </div>
   );
 }
