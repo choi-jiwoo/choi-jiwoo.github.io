8fb1e418d30c66e24433ee5f262543ad7ed2f18e
choi jiwoo <cho2.jiwoo@gmail.com>
2022-02-23 15:59:33 +0900

    Remove Cv.js component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavigationBar from './component/NavigationBar';
 import Footer from './component/Footer';
-import Cv from './page/Cv';
 import Home from './page/Home';
 import Project from './page/Project';
 import { pdfjs } from 'react-pdf';
@@ -16,11 +15,7 @@ function App() {
         <div className='content'>
           <Routes>
             <Route path='/choijiwoo' exact element={<Home />} />
-            <Route
-              path='/choijiwoo/cv'
-              exact
-              element={<Cv cv={'./cv.pdf'} />}
-            />
+            <Route path='/choijiwoo/cv.pdf' exact />
             <Route path='/choijiwoo/project' exact element={<Project />} />
           </Routes>
         </div>
