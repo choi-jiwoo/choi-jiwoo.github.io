6dea48cb9090002b46ee9d9dc3d361f64e51f756
choi-jiwoo <cho2.jiwoo@gmail.com>
2022-06-20 14:20:43 +0900

    add bottom padding for each education component

--- src/component/Education.js
+++ src/component/Education.js
@@ -9,18 +9,18 @@ function Education() {
           <div className='text-xl font-bold'>Ajou University</div>
           <div>2019~2022</div>
         </div>
-        <div className='pl-4 leading-7'>
+        <div className='pl-4 leading-7 pb-4'>
           <p className='italic'>
             - Bachelor of Business Administration in E-Business (Expected August
             2022)
           </p>
           <p>- GPA: 4.1 / 4.5</p>
         </div>
-        <div className='flex justify-between pt-4'>
+        <div className='flex justify-between'>
           <div className='text-xl font-bold'>University of San Carlos</div>
           <div>2013~2016</div>
         </div>
-        <div className='pl-4 leading-7'>
+        <div className='pl-4 leading-7 pb-4'>
           <p>
             - Completed 93 credits towards a B.S in Information and
             Communications Technology
