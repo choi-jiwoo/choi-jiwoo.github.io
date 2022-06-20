4123aa082705a015d40c13a490b91f73b64ee988
choi-jiwoo <cho2.jiwoo@gmail.com>
2022-06-20 14:20:20 +0900

    Improve About

--- src/component/About.js
+++ src/component/About.js
@@ -4,13 +4,17 @@ function About() {
   return (
     <div className='pt-20'>
       <div className='section-heading'>About</div>
-      <div className='leading-relaxed indent-8'>
-        Currently studying for a bachelor's degree in e-business at Ajou
-        University in Republic of Korea. Interested in tech industry generally,
-        data analytics, data engineering, and how technology and data can bring
-        about change. And I want to be involved in the great changes that are
-        happening now with big data and AI technologies in many other industries
-        not only in IT.
+      <div className='leading-relaxed'>
+        <p className='indent-8'>
+          Starting Master's in Industrial Engineering program in Yonsei
+          University from 2022 Fall semester. Interested in tech industry
+          generally, Data Science, Data Engineering, and how technology and data
+          can bring about change.
+        </p>
+        <p className='indent-8'>
+          I want to be involved in the great changes that are happening now with
+          big data and AI technologies in many other industries not only in IT.
+        </p>
       </div>
     </div>
   );
