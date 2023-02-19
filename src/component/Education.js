271a93bff5cd8a65816c7809e8e8db226a5c48d7
choi-jiwoo <cho2.jiwoo@gmail.com>
2023-02-20 01:13:02 +0900

    Update Education.js

--- src/component/Education.js
+++ src/component/Education.js
@@ -6,19 +6,27 @@ function Education() {
       <div className='section-heading'>Education</div>
       <div>
         <div className='flex justify-between'>
-          <div className='text-xl font-bold'>Ajou University</div>
-          <div>2019~2022</div>
+          <div className='text-xl font-bold'>Yonsei University</div>
+          <div>September 2022 ~ Present</div>
         </div>
         <div className='pl-4 leading-7 pb-4'>
           <p className='italic'>
-            - Bachelor of Business Administration in E-Business (Expected August
-            2022)
+            - Master of Science in Industrial Engineering (Expected August 2024)
+          </p>
+        </div>
+        <div className='flex justify-between'>
+          <div className='text-xl font-bold'>Ajou University</div>
+          <div>March 2019 ~ August 2022</div>
+        </div>
+        <div className='pl-4 leading-7 pb-4'>
+          <p className='italic'>
+            - Bachelor of Business Administration in E-Business
           </p>
           <p>- GPA: 4.1 / 4.5</p>
         </div>
         <div className='flex justify-between'>
           <div className='text-xl font-bold'>University of San Carlos</div>
-          <div>2013~2016</div>
+          <div>June 2013 ~ March 2016</div>
         </div>
         <div className='pl-4 leading-7 pb-4'>
           <p>
