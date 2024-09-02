c1452a276397a99c1242104a0cfa2b0e875dc14d
Choi Jiwoo <cho2.jiwoo@gmail.com>
2024-09-02 17:57:18 +0900

    Update to latest

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import NavigationBar from './component/NavigationBar';
 import Footer from './component/Footer';
 import Home from './page/Home';
 import Project from './page/Project';
+import ScrollToTop from './scrollToTop';
 
 function App() {
   return (
     <Router>
-      <div className='App'>
-        <NavigationBar />
+      <div className='App flex flex-col'>
+        <div className='sticky-nav border-b border-slate-600'>
+          <NavigationBar />
+        </div>
         <div className='content'>
+          <ScrollToTop />
           <Routes>
             <Route path='/choijiwoo' exact element={<Home />} />
             <Route path='/choijiwoo/cv.pdf' exact />
