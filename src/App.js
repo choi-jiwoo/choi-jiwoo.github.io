93ebd1a30b2c3bf07ade8d790ffd39a9ede8034b
Choi Jiwoo <cho2.jiwoo@gmail.com>
2024-09-03 01:02:52 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useEffect } from 'react';
 import NavigationBar from './component/NavigationBar';
 import Footer from './component/Footer';
 import Home from './page/Home';
 import Project from './page/Project';
-import ScrollToTop from './scrollToTop';
 
 function App() {
   return (
@@ -14,7 +12,6 @@ function App() {
           <NavigationBar />
         </div>
         <div className='content'>
-          {/* <ScrollToTop /> */}
           <Routes>
             <Route path='/choijiwoo' exact element={<Home />} />
             <Route path='/choijiwoo/cv.pdf' exact />
