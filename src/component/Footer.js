e1936677c4693b7c48fc64d0e2e294845371c75f
Choi Jiwoo <cho2.jiwoo@gmail.com>
2024-09-04 16:19:55 +0900

    Replace double quotes to sing quotes

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
 function Footer() {
   return (
-    <footer className='mt-auto text-center py-7 text-gray-400 text-center text-xs bg-gray-700'>
+    <footer className="mt-auto text-center py-7 text-gray-400 text-center text-xs bg-gray-700">
       <p>
-        Made with <span className='font-bold text-blue-500'>React</span> and
+        Made with <span className="font-bold text-blue-500">React</span> and
         hosted on
-        <span className='font-bold text-blue-500'> Github Pages</span>
+        <span className="font-bold text-blue-500"> Github Pages</span>
       </p>
       <p>Designed by Choi Ji Woo</p>
     </footer>
